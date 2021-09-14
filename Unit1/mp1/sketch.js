f7e3576977b1a1db91e2bd2ec07b83cebf44c832
dschwarz24 <djschw1@ilstu.edu>
2021-09-13 23:05:49 -0500

    caterpillar

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   createCanvas(1000, 800);
   rectMode(CENTER);
-  ellipseMode(CENTER);
+  ellipseMode(CENTER)
+  //  f1 = loadFont ("assets/Bertha.ttf") ;
+  // f2 = loadFont ("assets/Oswald.ttf") ;
+  textAlign (CENTER) ;
 
 }
 
@@ -9,35 +12,231 @@ function setup() {
 
 function draw() {
 
-  background('#A6DCF9');
+  background('#5EE8FA');
 
 
   if (mouseIsPressed) {
+
     // stuff that shows when the mouse is pressed
-    rect(200, 200, 200, 200);
+
+
+
+
+
+stroke('black') ;
+strokeWeight (4);
+line(500, 178, 468, 146);
+line(525, 179, 546, 142);
+
+noStroke() ;
+
+
+    fill('black') ;
+
+    ellipse(432, 300, 200, 200)
+    ellipse(435, 440, 170, 170)
+    ellipse(602, 300, 200, 200)
+    ellipse(599, 440, 170, 170)
+
+    fill ('#F5883D')
+    ellipse(432, 300, 150, 150)
+    ellipse(435, 440, 135, 135)
+    ellipse(602, 300, 150, 150)
+    ellipse(599, 440, 130, 130)
+    ellipse(480, 369, 130, 130)
+    ellipse(552, 369, 130, 130)
+
+    fill ('black') ;
+    ellipse(516, 350, 60, 350);
+    ellipse(515, 200, 50, 50);
+
+    fill('white') ;
+    ellipse(506, 194, 10, 10);
+    ellipse(525, 194, 10, 10);
+    ellipse(478, 223, 7, 7);
+    ellipse(453, 214, 7, 7);
+    ellipse(428, 211, 7, 7);
+    ellipse(400, 216, 7, 7);
+    ellipse(373, 231, 7, 7);
+     ellipse(358, 252, 7, 7);
+     ellipse(347, 274, 7, 7);
+     ellipse(343, 308, 7, 7);
+     ellipse(351, 336, 7, 7);
+     ellipse(369, 361, 7, 7);
+     ellipse(388, 375, 7, 7);
+     ellipse(372, 395, 7, 7);
+     ellipse(361, 419, 7, 7);
+     ellipse(358, 448, 7, 7);
+     ellipse(367, 477, 7, 7);
+     ellipse(382, 495, 7, 7);
+     ellipse(398, 507, 7, 7);
+     ellipse(420, 514, 7, 7);
+     ellipse(442, 514, 7, 7);
+     ellipse(461, 509, 7, 7);
+     ellipse(480, 498, 7, 7);
+
+     ellipse(551, 228, 7, 7);
+     ellipse(571, 217, 7, 7);
+     ellipse(591, 211, 7, 7);
+     ellipse(610, 211, 7, 7);
+     ellipse(629, 217, 7, 7);
+     ellipse(645, 224, 7, 7);
+     ellipse(659, 235, 7, 7);
+     ellipse(673, 246, 7, 7);
+     ellipse(682, 269, 7, 7);
+     ellipse(687, 297, 7, 7);
+     ellipse(685, 322, 7, 7);
+     ellipse(674, 344, 7, 7);
+     ellipse(661, 364, 7, 7);
+     ellipse(641, 375, 7, 7);
+     ellipse(658, 397, 7, 7);
+     ellipse(669, 418, 7, 7);
+     ellipse(672, 440, 7, 7);
+     ellipse(670, 461, 7, 7);
+     ellipse(660, 480, 7, 7);
+     ellipse(647, 496, 7, 7);
+     ellipse(630, 507, 7, 7);
+     ellipse(606, 514, 7, 7);
+     ellipse(581, 513, 7, 7);
+     ellipse(556, 500, 7, 7);
+     ellipse(544, 486, 7, 7);
+
+
+
+
+
+   fill('#01A838');
+
+    noStroke()
+  triangle(20, 621, -20, 797, 83, 797);
+  triangle(58, 631, 52,797, 136, 797);
+  triangle(97, 645, 55, 797, 184, 797);
+  triangle(150, 621, 106, 797, 221, 797);
+  triangle(203, 633, 273, 797, 163, 797);
+  triangle(260, 621, 314, 797, 201, 797);
+  triangle(351, 621, 229, 797, 367, 797);
+  triangle(294, 621, 409, 797, 248, 797);
+  triangle(394, 621, 441, 797, 288, 797);
+  triangle(430, 621, 501, 797, 362, 797);
+  triangle(462, 621, 541, 797, 456, 797);
+  triangle(511, 621, 570, 797, 456, 797);
+  triangle(555, 621, 646, 797, 512, 797);
+  triangle(613, 621, 694, 797, 516, 797);
+  triangle(655, 621, 745, 797, 622, 797);
+  triangle(695, 602, 801, 797, 622, 797);
+  triangle(732, 621, 845, 797, 729, 797);
+  triangle(788, 621, 829, 797, 700, 797);
+  triangle(840, 621, 892, 797, 759, 797);
+  triangle(887, 621, 892, 797, 759, 797);
+  triangle(924, 641, 947, 797, 834, 797);
+  triangle(964, 621, 980, 797, 858, 797);
+  triangle(1004, 621, 1014, 797,929, 797);
+
+
+// textFont (f1) ;
+ textSize (30) ;
+  fill ('black') ;
+
+ text ("One day he magically turned into a beautiful monarch butterfly!", width/2, 100) ;
+
+
+
   } else {
+
+
+
+
+
+
+
+
+
+
     // when the mouse isn't pressed!
+
+    background('#A6DCF9');
+    stroke('black') ;
+    strokeWeight (1);
     fill ('#00C458') ;
     ellipse(836, 339, 150, 150);
+
+    fill ('#58F50D') ;
     ellipse(745, 400, 150, 150) ;
+
+    fill ('#00C458') ;
     ellipse(633, 450, 150, 150) ;
+
+    fill ('#58F50D') ;
     ellipse(524, 432, 150, 150) ;
+
+    fill ('#00C458') ;
     ellipse(401, 404, 150, 150) ;
+
+    fill ('#58F50D') ;
     ellipse(305, 422, 150, 150) ;
+
+    fill ('#00C458') ;
     ellipse(200, 455, 150, 150) ;
+
+    fill ('#58F50D') ;
     ellipse(112, 439, 150, 150) ;
+
+
+    fill ('white') ;
+    ellipse(827, 321, 20, 20) ;
+    ellipse(873, 321, 20, 20) ;
+    fill ('black')
+    ellipse(827, 321, 12, 12) ;
+    ellipse(873, 321, 12, 12) ;
+    fill ('#F58194') ;
+    arc(848, 367, 35, 35, TWO_PI, PI) ;
+
+
+  fill('#188F4E');
+
+    noStroke() ;
+  triangle(20, 621, -20, 797, 83, 797);
+  triangle(58, 631, 52,797, 136, 797);
+  triangle(97, 645, 55, 797, 184, 797);
+  triangle(150, 621, 106, 797, 221, 797);
+  triangle(203, 633, 273, 797, 163, 797);
+  triangle(260, 621, 314, 797, 201, 797);
+  triangle(351, 621, 229, 797, 367, 797);
+  triangle(294, 621, 409, 797, 248, 797);
+  triangle(394, 621, 441, 797, 288, 797);
+  triangle(430, 621, 501, 797, 362, 797);
+  triangle(462, 621, 541, 797, 456, 797);
+  triangle(511, 621, 570, 797, 456, 797);
+  triangle(555, 621, 646, 797, 512, 797);
+  triangle(613, 621, 694, 797, 516, 797);
+  triangle(655, 621, 745, 797, 622, 797);
+  triangle(695, 602, 801, 797, 622, 797);
+  triangle(732, 621, 845, 797, 729, 797);
+  triangle(788, 621, 829, 797, 700, 797);
+  triangle(840, 621, 892, 797, 759, 797);
+  triangle(887, 621, 892, 797, 759, 797);
+  triangle(924, 641, 947, 797, 834, 797);
+  triangle(964, 621, 980, 797, 858, 797);
+  triangle(1004, 621, 1014, 797,929, 797);
+
+      // textFont (f1) ;
+ textSize (30) ;
+
+ text ("This is Fuzzy the caterpillar. He spends his day outside in the fresh air.", width/2, 100) ;
+
+
   }
 
 
 
   // this shows mouse location - comment it out when you're done!
 
-  fill(0);
-  text(mouseX + ", " + mouseY, 40, 40);
+ // fill(0);
+//  text(mouseX + ", " + mouseY, 40, 40);
 
 
 
-  text("", 80, 20);
+  //text("", 80, 20);
 
 }
 
